11dff8859619d43f96e2784af45bc8fadd1637cb
fernando718 <82851141+fernando718@users.noreply.github.com>
2021-05-12 21:10:47 -0500

    Create Ex3.c

---
+++ Ex3.c
@@ -0,0 +1,15 @@
+//VALDIVIA GONZALEZ JOSE FERNANDO// //4IV17//
+#include >stdio.h> 
+int main ()
+[
+   float fresa,precio, $13,cincokilogramos;
+   printf ("\n\n Programa 3");
+   printf ("\n\n precio de cualquier fruta $13");
+   printf ("\n\n ingrese la fruta que selecciono");
+   scanf ("%f",&fresa); 
+   printf ("\n\n seleccione el peso de la fruta seleccionada ");
+   scanf ("%f",&cincokilogramos); 
+   precio= cincokilogramos*$13
+   printf  ("\n\n el precio es multiplicar cinco kilogramos por trece pesos es &f");
+   return 0;
+] 
